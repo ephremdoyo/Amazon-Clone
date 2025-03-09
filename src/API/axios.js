80b3c0d2746ec2c5fff85d5dd4d9625fb24f1408
ephremdoy <ephremdoyo@gmail.com>
2025-03-09 20:50:25 +0300

    api updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -4,7 +4,10 @@ const axiosInstace = axios.create({
   // local instance of firebase functions
   // baseURL: "http://127.0.0.1:5001/clone-109d8/us-central1/api",
   // deployed version of amazon server on renderMatches.com 
+  // baseURL: "http://localhost:5000/",
   baseURL: "https://amazon-api-deploy-ijd9.onrender.com",
 });
 
 export {axiosInstace}
+
+
